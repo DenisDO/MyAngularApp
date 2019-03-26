13384ee607f55b866fa830b2dce18d47e7f9dc0c
Denis Derkach <podstavka128@gmail.com>
2019-03-26 15:35:24 +0200

    fix/home, use forkJoin to do some requests

--- src/app/pages/home-page/home.page.component.ts
+++ src/app/pages/home-page/home.page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUsers } from 'src/app/shared/interfaces/users.interface';
 import { HomePageService } from './home.page.service';
 import { Router } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
     selector: 'app-home-page',
@@ -9,15 +10,16 @@ import { Router } from '@angular/router';
     styleUrls: ['home.page.component.scss']
 })
 export class HomeComponent implements OnInit {
-    usersList: IUsers;
+    usersList: IUsers[];
     columnsToDisplay = ['userID', 'userAvatar', 'userName', 'userSurname'];
 
     constructor(private homePageService: HomePageService, private router: Router) { }
 
     ngOnInit(): void {
-        this.homePageService.getUsersList().subscribe(data => {
-            this.usersList = data;
-        });
+        forkJoin(this.homePageService.getUsersList())
+            .subscribe(data => {
+                this.usersList = this.homePageService.formatUserData(data);
+            });
     }
 
     logOut(): void {
--- src/app/pages/home-page/home.page.service.ts
+++ src/app/pages/home-page/home.page.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { IUsers } from 'src/app/shared/interfaces/users.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { USERS_BASE_API_URL } from '../../shared/config';
 
 @Injectable()
 export class HomePageService {
     constructor(private http: HttpClient) {}
 
-    getUsersList(): Observable<IUsers> {
-        return this.http.get<IUsers>(`${USERS_BASE_API_URL}/users?page=2`);
+    getUsersList(): Observable<IUsers>[] {
+        const pages: number[] = [1, 2, 3, 4];
+
+        return pages.map<Observable<IUsers>>(page => this.http.get<IUsers>(`${USERS_BASE_API_URL}/users?page=${page}`));
+    }
+
+    formatUserData(data): [] {
+        return data.map(user => user.data).flat();
     }
 }
